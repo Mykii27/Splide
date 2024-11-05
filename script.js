519649c9f024082ad5b2cc43ce910fbb3c346de5
Mykii27 <96043992+Mykii27@users.noreply.github.com>
2024-11-05 23:58:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
-var splide = new Splide( '.slider1', {
-  type   : 'loop',
-  padding: '5rem',
-} );
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".slider1", {
+    type: "loop",
+    padding: "5rem",
+  });
+  splide.mount();
+});
 
-splide.mount();
