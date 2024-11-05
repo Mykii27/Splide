b04ebec054fd0b12c77f34074cf845a908b89c48
Mykii27 <96043992+Mykii27@users.noreply.github.com>
2024-11-06 00:21:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+
   document.addEventListener("DOMContentLoaded", function () {
+    console.log("Inline script loaded");  // Check if the inline script runs
     var splide = new Splide(".slider1", {
       type: "loop",
       padding: "5rem",
     });
     splide.mount();
+    console.log("Inline script: Splide slider initialized");
   });
